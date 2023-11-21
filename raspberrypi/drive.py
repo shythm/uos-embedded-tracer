5f21555d6a4be307cd834893c304ebce8e7ccba8
Seongho Lee <shythm@outlook.com>
2023-11-21 23:59:32 +0900

    refactor: exception handling, add constants

--- raspberrypi/drive.py
+++ raspberrypi/drive.py
@@ -1,15 +1,25 @@
 import serial
 
+PORT = '/dev/ttyAMA0'
+BAUDRATE = 9600
+
 ser = serial.Serial(
-    port='/dev/ttyAMA0',
-    baudrate=9600,
+    port=PORT,
+    baudrate=BAUDRATE,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
     timeout=1
 )
 
-def set_power(left, right):
+def set_power(left: int, right: int):
+    # the power is between 0 and 255
+    left = max(0, left)
+    left = min(left, 255)
+    right = max(0, right)
+    right = min(right, 255)
+
+    # send command
     cmd = 'L{:+03d}\n'.format(left)
     ser.write(cmd.encode())
     cmd = 'R{:+03d}\n'.format(right)
