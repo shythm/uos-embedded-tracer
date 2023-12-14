ee90b196128836ddcb48b7d4b89ae3a109dd0209
Seongho Lee <tjdgh2626@gmail.com>
2023-12-14 12:07:05 +0900

    Add immediate method

--- raspberrypi/sensing.py
+++ raspberrypi/sensing.py
@@ -7,7 +7,7 @@ SCALEDOWN_HEIGHT = 30
 BINARY_THRESHOLD = 128
 BASE_LINE = int(SCALEDOWN_HEIGHT * 0.5)
 
-def sense_line(img):
+def _get_pos_arr(img):
     # resize image
     img = cv2.resize(img, (SCALEDOWN_WIDTH, SCALEDOWN_HEIGHT))
 
@@ -26,21 +26,30 @@ def sense_line(img):
     # get position array
     pos_arr = base_line_array > 0
 
-    # get next position
-    pos = _get_next_position(pos_arr)
-
-    return pos, img
+    return img, pos_arr
 
 _prev_pos = -1
+
+def _get_next_position_immediate(pos_arr) -> int:
+    detected = np.where(pos_arr)
+    if len(detected) == 0:
+        # no line detected
+        return _prev_pos
+    else:
+        # get mean of detected positions
+        mean_pos = int(np.mean(detected))
+        _prev_pos = mean_pos
+        return mean_pos
+
 SCORE_THRESHOLD = SCALEDOWN_WIDTH * 0.2
 
-def _get_next_position(arr):
+def _get_next_position_hueristic(pos_arr) -> int:
     global _prev_pos
 
     max_score = -math.inf
     max_pos = -1
 
-    for pos, detected in enumerate(arr):
+    for pos, detected in enumerate(pos_arr):
         if detected:
             # calculate hueristic score
             score = -abs(_prev_pos - pos)
@@ -54,3 +63,18 @@ def _get_next_position(arr):
 
     _prev_pos = max_pos # update prev_pos
     return max_pos
+
+def sense_line(img, method) -> float:
+    im, pos_arr = _get_pos_arr(img)
+
+    if method == 'immediate':
+        pos = _get_next_position_immediate(pos_arr)
+    elif method == 'hueristic':
+        pos = _get_next_position_hueristic(pos_arr)
+    else:
+        raise ValueError('Invalid method')
+
+    # centering
+    pos = (pos - SCALEDOWN_WIDTH * 0.5) / (SCALEDOWN_WIDTH * 0.5)
+
+    return im, pos
