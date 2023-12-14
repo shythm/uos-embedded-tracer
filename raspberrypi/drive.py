6aedf2741bbae2c13a75fe60379cc27b0ba52898
Seongho Lee <tjdgh2626@gmail.com>
2023-12-14 12:06:18 +0900

    Add min max power

--- raspberrypi/drive.py
+++ raspberrypi/drive.py
@@ -12,12 +12,19 @@ ser = serial.Serial(
     timeout=1
 )
 
+MIN_POWER = 100
+MAX_POWER = 255
+
 def set_power(left: int, right: int):
+    # map power [0, 255] to [MIN_POWER, MAX_POWER]
+    left = int((left - MIN_POWER) / (MAX_POWER - MIN_POWER) * 255)
+    right = int((right - MIN_POWER) / (MAX_POWER - MIN_POWER) * 255)
+
     # the power is between 0 and 255
-    left = max(0, left)
-    left = min(left, 255)
-    right = max(0, right)
-    right = min(right, 255)
+    left = max(MIN_POWER, left)
+    left = min(left, MAX_POWER)
+    right = max(MIN_POWER, right)
+    right = min(right, MAX_POWER)
 
     # send command
     cmd = 'L{:+03d}\n'.format(left)
